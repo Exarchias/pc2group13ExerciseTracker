f909a8ade28ca7199290df580cba973dbf0733d8
Robert Kristian Alm <exarchiasghost@gmail.com>
2019-04-24 01:44:41 +0200

    A primitive login system is implemented
    next logical step is to take care for a log out system. Also it will be necessary to add labels on scenes 2 and 3.

--- ExerciseTracker/src/sample/Controller1.java
+++ ExerciseTracker/src/sample/Controller1.java
@@ -26,19 +26,44 @@ public class Controller1 implements Initializable { //LOGIN PAGE
 
     @FXML
     public void btnAdminPressed(ActionEvent event) throws Exception{
-        Main.getInstance().setScene(Main.Scene2);
+        DataHolder.setAdmin(true);
+        System.out.println("Now you are Credible for Admin");
 
 
     }
     @FXML
     public void btnUserPressed(ActionEvent event)throws Exception{
-        Main.getInstance().setScene(Main.Scene3);
+        DataHolder.setAdmin(false);
+        System.out.println("Now you are NOT Credible for Admin");
+
+    }
+
+    @FXML
+    public void btnLoginIsTrue(ActionEvent event)throws Exception{
+        DataHolder.setLogin(true);
+        System.out.println("Now you are Credible to Log In");
+
+    }
+
+    @FXML
+    public void btnLoginIsFalse(ActionEvent event)throws Exception{
+        DataHolder.setLogin(false);
+        System.out.println("Now your credentials are invalid");
 
     }
 
     @FXML
     public void btnLogin(ActionEvent event)throws Exception{
-        Main.getInstance().setScene(Main.Scene3);
+        if(DataHolder.isLogin()){
+            if(DataHolder.isAdmin()){
+                Main.getInstance().setScene(Main.Scene3);
+            } else {
+                Main.getInstance().setScene(Main.Scene2);
+            }
+        } else {
+            System.out.println("Access denied!");
+        }
+
 
     }
 
