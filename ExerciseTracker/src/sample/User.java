1da8225853138292f99386e12cca19fc5cd47c75
Robert Kristian Alm <exarchiasghost@gmail.com>
2019-04-26 01:59:09 +0200

    filling User class with attributes
    in order to hanfle the user we will have to instantiate User objects, and this is why I needed at least 4 of the necessary attributes.
    Probably whould be correct to have all the attributes at once, but this would make the code overcomplicated and I haven't worked with the comments yet.

--- ExerciseTracker/src/sample/User.java
+++ ExerciseTracker/src/sample/User.java
@@ -1,5 +1,47 @@
 package sample;
 
 public class User {
+    private String userName;
+    private String passWord;
+    private String email;
+    private boolean isAnAdmin;
 
+    public User(String userName, String passWord, String email, Boolean isAdmin){
+        this.userName = userName;
+        this.passWord = passWord;
+        this.email = email;
+        this.isAnAdmin = isAdmin;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassWord() {
+        return passWord;
+    }
+
+    public void setPassWord(String passWord) {
+        this.passWord = passWord;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isAnAdmin() {
+        return isAnAdmin;
+    }
+
+    public void setAnAdmin(boolean anAdmin) {
+        isAnAdmin = anAdmin;
+    }
 }
