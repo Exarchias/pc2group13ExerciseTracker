daf516b93ec87a9b02e4f36b6b33ef547000ef59
Sumit Dubey <nordicandersson@gmail.com>
2019-04-25 10:17:55 +0200

    MysqlCon.java code updated

--- ExerciseTracker/src/sample/DriverManager.java
+++
@@ -1,7 +0,0 @@
-public class DBconnection {
-
-    Connection connection = DriverManager.getConnection(jdbc:mysql://127.0.0.1:3306/ExerciseTrackerJDBC?user=root&password= root);
-    Statement st = connection.createStatement();
-    ResultSet rs= st.executeQuery("SELECT* FROM ExerciseTrackerJDBC");
-
-}
