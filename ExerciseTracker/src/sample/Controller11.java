2d0a2151ade1ea5046587118afc73892b5f3d29e
Lavdim Imeri <lavdi-imeri@hotmail.com>
2019-05-07 23:31:56 +0200

    Button Go to the admin page hidden if you Log in as a User(Scene11 (Exercise Page))

--- ExerciseTracker/src/sample/Controller11.java
+++ ExerciseTracker/src/sample/Controller11.java
@@ -3,6 +3,7 @@ package sample;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
@@ -28,8 +29,14 @@ public class Controller11 implements Initializable { //EXERCISE PAGE
         titleTextField.setText(DataHolder.supervisedExercise.getTitle());
         descriptionTextArea.setText(DataHolder.supervisedExercise.getDescription());
 
+        if (DataHolder.isAdmin())
+            btnGoToAdminPage.setVisible(true);
+        else btnGoToAdminPage.setVisible(false);
+
 
     }
+    @FXML
+    Button btnGoToAdminPage;
 
     @FXML
     TextField titleTextField;
