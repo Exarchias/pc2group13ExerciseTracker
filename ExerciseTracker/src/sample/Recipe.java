791045a3b4045795edca25db99901774d2f859b5
raghudeepbathula <raghudeep.bathula98@gmail.com>
2019-05-16 19:05:30 +0200

    Adjusting the Recipe Class
    I made the Recipe class more EER friendly.
    I added the recipeID toRecipe class.
    I created a new constructor that includes all the variables.

--- ExerciseTracker/src/sample/Recipe.java
+++ ExerciseTracker/src/sample/Recipe.java
@@ -6,7 +6,16 @@ public class Recipe {
     private String title;
     private String description;
     private int owner;
+    private int recipeID;
     ArrayList<Exercise> exerciseList = new ArrayList<>();
+
+    //this constructor is according to the EER.
+    public Recipe(String title, String description, int owner, int recipeID) {
+        this.title = title;
+        this.description = description;
+        this.owner = owner;
+        this.recipeID = recipeID;
+    }
 
     public Recipe(String title, String description, int owner){
         this.title = title;
@@ -28,5 +37,21 @@ public class Recipe {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public int getOwner() {
+        return owner;
+    }
+
+    public void setOwner(int owner) {
+        this.owner = owner;
+    }
+
+    public int getRecipeID() {
+        return recipeID;
+    }
+
+    public void setRecipeID(int recipeID) {
+        this.recipeID = recipeID;
     }
 }
