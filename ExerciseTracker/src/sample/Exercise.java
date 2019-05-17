d92002d9e57ead6ad2ebebebf6d6bd297434d719
raghudeepbathula <raghudeep.bathula98@gmail.com>
2019-05-17 17:46:44 +0200

    Adjustment of the Exercise Class
    I added more variables to the Exercise Class to make it more EER friendly.
    I added a new constructor that includes all the new variables and the necessary setters and getters.

--- ExerciseTracker/src/sample/Exercise.java
+++ ExerciseTracker/src/sample/Exercise.java
@@ -5,12 +5,27 @@ package  sample;
 public class Exercise {
     private String title;
     private String description;
+    private int exerciseID;
     private int owner;
+    private int recipeID;
+    private int repetitions;
+    private int caloriesPerRepetition;
 
     public Exercise(String title, String description, int owner){
         this.title = title;
         this.description = description;
         this.owner = owner;
+    }
+
+    public Exercise(String title, String description, int exerciseID,
+                    int owner, int recipeID, int repetitions, int caloriesPerRepetition) {
+        this.title = title;
+        this.description = description;
+        this.exerciseID = exerciseID;
+        this.owner = owner;
+        this.recipeID = recipeID;
+        this.repetitions = repetitions;
+        this.caloriesPerRepetition = caloriesPerRepetition;
     }
 
     public String getTitle() {
@@ -35,5 +50,37 @@ public class Exercise {
 
     public void setOwner(int owner) {
         this.owner = owner;
+    }
+
+    public int getExerciseID() {
+        return exerciseID;
+    }
+
+    public void setExerciseID(int exerciseID) {
+        this.exerciseID = exerciseID;
+    }
+
+    public int getRecipeID() {
+        return recipeID;
+    }
+
+    public void setRecipeID(int recipeID) {
+        this.recipeID = recipeID;
+    }
+
+    public int getRepetitions() {
+        return repetitions;
+    }
+
+    public void setRepetitions(int repetitions) {
+        this.repetitions = repetitions;
+    }
+
+    public int getCaloriesPerRepetition() {
+        return caloriesPerRepetition;
+    }
+
+    public void setCaloriesPerRepetition(int caloriesPerRepetition) {
+        this.caloriesPerRepetition = caloriesPerRepetition;
     }
 }
