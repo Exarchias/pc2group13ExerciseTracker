4fffceff5b4c091198435ec382e0b586ce7ad42e
Sumit Dubey <45233901+nordicandersson@users.noreply.github.com>
2019-06-03 23:47:39 +0200

    Adding first method - creating new PDF document

--- ExerciseTracker/src/sample/PDF.java
+++ ExerciseTracker/src/sample/PDF.java
@@ -9,3 +9,12 @@ import com.qoppa.pdfWriter.PDFDocument;
 import com.qoppa.pdfWriter.PDFGraphics;
 import com.qoppa.pdfWriter.PDFPage;
 
+public class PDF
+{
+    public static void PdfFun()
+    {
+        try
+        {
+            // Create a document and a page in default Locale format
+            PDFDocument pdfDoc = new PDFDocument();
+            PDFPage newPage = pdfDoc.createPage(new PageFormat());
