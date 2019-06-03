f80456a0baedeb0fd95cbc2c7917336be926e01d
Robert Kristian Alm <exarchiasghost@gmail.com>
2019-06-04 00:55:27 +0200

    Confirmed PDF generation
    The program just made its first pdf. The pdf feature works.

--- ExerciseTracker/src/sample/Main.java
+++ ExerciseTracker/src/sample/Main.java
@@ -53,6 +53,7 @@ public class Main extends Application {
 
     }
     public static void main(String[] args) throws NoSuchAlgorithmException {
+        //PDF.PdfFun(); //TESTING REASONS
         if(DataHolder.dbActivated){ //CHECKS IF THE DATA BASE IS ACTIVATED
             //JDBC starts
             DB db = new DB();
