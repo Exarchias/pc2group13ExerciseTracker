5f78c43c4dc7c9f4aad6bd2ad33475c2df523976
Robert Kristian Alm <exarchiasghost@gmail.com>
2019-06-08 04:19:04 +0200

    Inheritance
    now the Template class is a child class of the Exercise class.

--- ExerciseTracker/src/sample/Template.java
+++ ExerciseTracker/src/sample/Template.java
@@ -1,37 +1,42 @@
 package sample;
 
-public class Template {
+public class Template extends Exercise {
     private  int templateID;
-    private String title;
-    private String description;
+    //private String title;
+    //private String description;
 
-    public Template(int templateID, String title, String description) {
-        this.templateID = templateID;
-        this.title = title;
-        this.description = description;
-    }
-
-    public int getTemplateID() {
-        return templateID;
-    }
-
-    public void setTemplateID(int templateID) {
+    public Template(String title, String description, int owner, int templateID) {
+        super(title, description, owner);
         this.templateID = templateID;
     }
 
-    public String getTitle() {
-        return title;
-    }
+//    public Template(int templateID, String title, String description) {
+//        this.templateID = templateID;
+//        this.title = title;
+//        this.description = description;
+//    }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
+//    public int getTemplateID() {
+//        return templateID;
+//    }
+//
+//    public void setTemplateID(int templateID) {
+//        this.templateID = templateID;
+//    }
+//
+//    public String getTitle() {
+//        return title;
+//    }
+//
+//    public void setTitle(String title) {
+//        this.title = title;
+//    }
+//
+//    public String getDescription() {
+//        return description;
+//    }
+//
+//    public void setDescription(String description) {
+//        this.description = description;
+//    }
 }
